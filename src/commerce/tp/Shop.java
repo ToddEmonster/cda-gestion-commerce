e7aa09450c95d8788acf6ae3896c3fd966f92964
Todd Raharivelo <t.raharivelo@outlook.fr>
2021-12-09 10:37:50 +0100

    feat: set Product class

--- src/commerce/tp/Shop.java
+++ src/commerce/tp/Shop.java
@@ -1,4 +1,45 @@
 package commerce.tp;
 
 public class Shop {
+    private String label;
+    private float buyingPrice;
+    private float sellingPrice;
+    private int stock;
+    private String description;
+
+    public Shop(String label, float buyingPrice, float sellingPrice) {
+        this.label = label;
+        this.buyingPrice = buyingPrice;
+        this.sellingPrice = sellingPrice;
+        this.stock = 0;
+        this.description = "Pas de description";
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public float getBuyingPrice() {
+        return buyingPrice;
+    }
+
+    public float getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
